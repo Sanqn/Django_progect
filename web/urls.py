f0bcfb5db363ce399394ff4fd84e303e043db4e5
Sanqn <sandisvert@gmail.com>
2021-12-18 18:47:03 +0300

    Django_check_day

--- web/urls.py
+++ web/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.index),  # dynamic URLS
     path('type/', views.type),
     path('type/<type_sign>/', views.get_type_sign, name='url_type'),
+    path('<int:month>/<int:day>/', views.check_day),
     path('<int:sigen_zodiac>/', views.get_more_sigen_zodiac_by_number),  # dynamic URLS
     path('<str:sigen_zodiac>/', views.get_more_sigen_zodiac, name='url_revers'), # dynamic URLS
     # path('leo', views.leo),
--- web/views.py
+++ web/views.py
@@ -16,25 +16,137 @@ from django.urls import reverse
 #
 # def gemini(request):
 #     return HttpResponse('Zodiac sign Gemini')
-zodiac = {"aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)",
-          "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)",
-          "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)",
-          "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля)",
-          "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа)",
-          "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября)",
-          "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября)",
-          "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября)",
-          "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря)",
-          "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января",
-          "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля)",
-          "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)"
-          }
-
+# zodiac = {"aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)",
+#           "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)",
+#           "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)",
+#           "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля)",
+#           "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа)",
+#           "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября)",
+#           "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября)",
+#           "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября)",
+#           "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря)",
+#           "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января",
+#           "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля)",
+#           "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)"
+#           }
+#
 types = {
     'fire': ['aries', 'leo', 'sagittarius'],
     'ears': ['taurus', 'virgo', 'capricorn'],
     'air': ['gemini', 'libra', 'aquarius'],
     'water': ['cancer', 'scorpio', 'pisces'],
+}
+
+zodiac = {
+    'aries':
+        {'description': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+         'types': 'fire',
+         'day_start': 21,
+         'month_start': 3,
+         'day_finish': 20,
+         'month_finish': 4,
+         'si': 'aries'
+         },
+    'taurus':
+        {'description': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+         'types': 'earth',
+         'day_start': 21,
+         'month_start': 4,
+         'day_finish': 21,
+         'month_finish': 5,
+         'si': 'taurus'
+         },
+    'gemini':
+        {'description': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+         'types': 'air',
+         'day_start': 22,
+         'month_start': 5,
+         'day_finish': 21,
+         'month_finish': 6,
+         'si': 'gemini'
+         },
+    'cancer':
+        {'description': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+         'types': 'water',
+         'day_start': 22,
+         'month_start': 6,
+         'day_finish': 22,
+         'month_finish': 7,
+         'si': 'cancer'
+         },
+    'leo':
+        {'description': 'Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+         'types': 'fire',
+         'day_start': 23,
+         'month_start': 7,
+         'day_finish': 21,
+         'month_finish': 8,
+         'si': 'leo'
+         },
+    'virgo':
+        {'description': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+         'types': 'earth',
+         'day_start': 22,
+         'month_start': 8,
+         'day_finish': 23,
+         'month_finish': 9,
+         'si': 'virgo'
+         },
+    'libra':
+        {'description': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+         'types': 'air',
+         'day_start': 24,
+         'month_start': 9,
+         'day_finish': 23,
+         'month_finish': 10,
+         'si': 'libra'
+         },
+    'scorpio':
+        {'description': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+         'types': 'water',
+         'day_start': 24,
+         'month_start': 10,
+         'day_finish': 22,
+         'month_finish': 11,
+         'si': 'scorpio'
+         },
+    'sagittarius':
+        {'description': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+         'types': 'fire',
+         'day_start': 23,
+         'month_start': 11,
+         'day_finish': 22,
+         'month_finish': 12,
+         'si': 'sagittarius'
+         },
+    'capricorn':
+        {'description': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+         'types': 'earth',
+         'day_start': 23,
+         'month_start': 12,
+         'day_finish': 20,
+         'month_finish': 1,
+         'si': 'capricorn'
+
+         },
+    'aquarius':
+        {'description': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+         'types': 'air',
+         'day_start': 21,
+         'month_start': 1,
+         'day_finish': 19,
+         'month_finish': 2,
+         'si': 'aquarius'
+         },
+    'pisces':
+        {'description': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+         'types': 'water',
+         'day_start': 20,
+         'month_start': 2,
+         'day_finish': 20,
+         'month_finish': 3,
+         'si': 'pisces'
+         }
 }
 
 def index(request):
@@ -52,7 +164,9 @@ def index(request):
 
 
 def get_more_sigen_zodiac(request, sigen_zodiac: str):  # dynamic URLS
-    description = zodiac.get(sigen_zodiac, None)
+    # description = zodiac.get([sigen_zodiac[sign]['description'] for sign in sigen_zodiac], None)
+    all = zodiac.get(sigen_zodiac, None)
+    description = all.get('description', None)
     if description:
         return HttpResponse(f'<h2>{description}</h2>')
     # if sigen_zodiac == 'leo':
@@ -81,7 +195,7 @@ def type(request):
     list_type_sign = ''
     for typ in type_sign:
         link_tupe_sign = reverse('url_type', args=[typ])
-        list_type_sign += f"<li><a href='{link_tupe_sign}'>{typ}</a></li>"
+        list_type_sign += f"<li><a href='{link_tupe_sign}'>{typ.title()}</a></li>"
     resource = f'''
     <ul>
         {list_type_sign}
@@ -89,18 +203,19 @@ def type(request):
     '''
     return HttpResponse(resource)
 
+
 def get_type_sign(request, type_sign):
     sign_of_type = types.get(type_sign, None)
     if sign_of_type:
         list_type_one_sign = ''
         for four in sign_of_type:
             link_tupe_one_sign = reverse('url_revers', args=[four])
-            list_type_one_sign += f"<li><a href='{link_tupe_one_sign}'>{four}</a></li>"
+            list_type_one_sign += f"<li><a href='{link_tupe_one_sign}'>{four.title()}</a></li>"
         resource = f'''
-            <ul>
-                {list_type_one_sign}
-            </ul>
-            '''
+        <ul>
+            {list_type_one_sign}
+        </ul>
+        '''
         return HttpResponse(resource)
 
 # def get_four_digits(request, sigen_zodiac):
@@ -109,3 +224,12 @@ def get_type_sign(request, type_sign):
 #
 # def get_date(request, sigen_zodiac):
 #     return HttpResponse(f'This date is {sigen_zodiac}')
+
+def check_day(request, month, day):
+    if day > 31 or month > 12:
+        return HttpResponseNotFound(f'<h2>This page not found</h2>')
+    else:
+        for i in zodiac:
+            if (day >= zodiac[i]['day_start'] and month == zodiac[i]['month_start']) or (day <= zodiac[i]['day_finish'] and month == zodiac[i]['month_finish']):
+                return HttpResponse(f"<h2>{zodiac[i]['si']}</h2>")
+
