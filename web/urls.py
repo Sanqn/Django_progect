884fc4b10c6d7492e3bdb08b231fde675403c4db
Sanqn <sandisvert@gmail.com>
2021-12-27 22:27:20 +0300

    Django_static

--- web/urls.py
+++ web/urls.py
@@ -7,7 +7,7 @@ register_converter(converters.MydateConverter, 'my_date')
 urlpatterns = [
     # path('<my_date:sigen_zodiac>/', views.get_date),  # dynamic URLS
     # path('<yyyy:sigen_zodiac>/', views.get_four_digits),  # dynamic URLS
-    path('', views.index),  # dynamic URLS
+    path('', views.index, name= 'url_index'),  # dynamic URLS
     path('type/', views.type),
     path('type/<type_sign>/', views.get_type_sign, name='url_type'),
     path('<int:month>/<int:day>/', views.check_day),
